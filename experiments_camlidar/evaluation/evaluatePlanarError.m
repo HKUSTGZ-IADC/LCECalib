146352b1743d0d0a647ba77e6d2e0674ce7bfd86
jjiao <jiaojh1994@gmail.com>
2022-05-26 10:51:15 +0800

    use evaluateTotalPlanarError as the criteria

--- experiments_camlidar/evaluation/evaluatePlanarError.m
+++
@@ -1,6 +0,0 @@
-function [err, cnt] = evaluatePlanarError(T_est, cam_plane_coeff, lidar_pts)
-  cam_pts = T_est(1:3, 1:3) * lidar_pts + T_est(1:3, 4);
-  d = cam_plane_coeff(1:3)' * cam_pts + cam_plane_coeff(4);
-  err = sum(abs(d));
-  cnt = size(d, 2);
-end
---
+++ experiments_camlidar/evaluation/evaluateTotalPlanarError.m
@@ -0,0 +1,9 @@
+% input: 
+% output: err: the total error by summing all absolute planar error terms
+%         cnt: number of planar error terms
+function [err, cnt] = evaluateTotalPlanarError(T_est, cam_plane_coeff, lidar_pts)
+  cam_pts = T_est(1:3, 1:3) * lidar_pts + T_est(1:3, 4);
+  d = cam_plane_coeff(1:3)' * cam_pts + cam_plane_coeff(4);
+  err = sum(abs(d));
+  cnt = size(d, 2);
+end
